49f272f4658b56c37a8aa0b17216f4ae400b8575
mialemonica <mia.lemonica@gmail.com>
2026-06-12 09:15:32 -0400

    worked on the color changing aspect of the box, only did red

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ const clicky = new Audio("sfx/click.mp3");
 clicky.play();
 
 var box = document.getElementById("box");
+var innerBox = document.querySelector(".box2");
 var bounds = document.getElementById("speedmeter2");
-var coordx = document.getElementById("coordx");
-var coordy = document.getElementById("coordy");
+var coordx = document.querySelector(".coordx p");
+var coordy = document.querySelector(".coordy p");
+
+
+
+function colorFromWave(x) {
+    var y = 252 / (Math.pow(Math.abs(5), 5 * Math.sin(0.015 * x + 0.5)) + 1) - 1.5;
+    var red = Math.round(Math.max(0, Math.min(255, y * 10)));
+
+    return "rgb(" + red + ", 0, 0)";
+}
+
+function updateInnerBoxColor(left) {
+    if (!innerBox) return;
+
+    innerBox.style.backgroundColor = colorFromWave(left);
+}
 
 function drag(element) {
     var dragOffsetX = 0;
     var dragOffsetY = 0;
 
     element.addEventListener("mousedown", dragMouseDown);
+
+    element.addEventListener("touchstart", dragTouchDown);
 
     function dragMouseDown(e) {
         e.preventDefault();
@@ -22,6 +40,16 @@ function drag(element) {
 
         document.addEventListener("mousemove", elementDrag);
         document.addEventListener("mouseup", close);
+    }
+
+    function dragTouchDown(e) {
+        e.preventDefault();
+        var touch = e.touches[0];
+        var rect = element.getBoundingClientRect();
+        dragOffsetX = touch.clientX - rect.left - (bounds.getBoundingClientRect().left / 2);
+        dragOffsetY = touch.clientY - rect.top - (bounds.getBoundingClientRect().top / 2);
+        document.addEventListener("touchmove", elementDrag);
+        document.addEventListener("touchend", close);
     }
 
     function elementDrag(e) {
@@ -40,11 +68,27 @@ function drag(element) {
 
         element.style.left = nextLeft + "px";
         element.style.top = nextTop + "px";
+
+        updateInnerBoxColor(
+            nextLeft,
+            nextTop,
+            containerRect.width - element.offsetWidth,
+            containerRect.height - element.offsetHeight
+        );
+
+        var centeredX = nextLeft - (containerRect.width / 2);
+        var centeredY = nextTop - (containerRect.height / 2);
+
+        if (coordx) coordx.textContent = "X: " + (Math.round(centeredX) + 10);
+        if (coordy) coordy.textContent = "Y: " + (Math.round(centeredY) + 10);
+
     }
 
     function close() {
         document.removeEventListener("mousemove", elementDrag);
         document.removeEventListener("mouseup", close);
+        document.removeEventListener("touchmove", elementDrag);
+        document.removeEventListener("touchend", close);
     }
 }
 
